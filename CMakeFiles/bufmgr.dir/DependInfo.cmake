a60a40abe7e960462d2cb56a9368e524a2352859
Gregory  Zabrodskiy <grzabrodskiy@gmail.com>
2020-12-14 20:40:10 +0100

    first commit

---
+++ CMakeFiles/bufmgr.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/alex/Documents/workspace/BufManager/bmtest.cpp" "/Users/alex/Documents/workspace/BufManager-bin/CMakeFiles/bufmgr.dir/bmtest.cpp.o"
+  "/Users/alex/Documents/workspace/BufManager/bufmgr.cpp" "/Users/alex/Documents/workspace/BufManager-bin/CMakeFiles/bufmgr.dir/bufmgr.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
